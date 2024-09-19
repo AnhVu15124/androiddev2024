f88a3cc9706157f54e25767cc41df7df4456dccd
Tran Anh Vu <tav04092004@gmail.com>
2024-09-19 21:13:23 +0700

    Finished prac12

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,19 +1,34 @@
 package vn.edu.usth.weather;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
 import com.google.android.material.tabs.TabLayout;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
+    private static final int REQUEST_WRITE_PERMISSION = 100;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,61 @@ public class WeatherActivity extends AppCompatActivity {
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         Log.i(TAG, "onCreate() called");
+
+        // Request write permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        } else {
+            extractAndPlayMusic();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_WRITE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                extractAndPlayMusic();
+            }
+        }
+    }
+
+    private void extractAndPlayMusic() {
+        try {
+            // Extract the MP3 file to the external storage
+            File musicFile = new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC), "music.mp3");
+            if (!musicFile.exists()) {
+                InputStream is = getResources().openRawResource(R.raw.music);
+                OutputStream os = new FileOutputStream(musicFile);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, length);
+                }
+
+                os.flush();
+                os.close();
+                is.close();
+                Log.i(TAG, "MP3 file extracted to: " + musicFile.getAbsolutePath());
+            }
+
+            // Play the music file
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(musicFile.getAbsolutePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            Log.i(TAG, "Playing the extracted MP3 file");
+
+        } catch (Exception e) {
+            Log.e(TAG, "Error extracting or playing music file", e);
+        }
     }
 
     @Override
@@ -46,12 +115,20 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "onPause() called");
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop() called");
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
     @Override
@@ -60,7 +137,6 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i(TAG, "onDestroy() called");
     }
 
-    // Inner class for the PagerAdapter
     private class WeatherPagerAdapter extends FragmentPagerAdapter {
         private final String[] tabTitles = getResources().getStringArray(R.array.tab_titles);
 
@@ -83,5 +159,21 @@ public class WeatherActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return tabTitles[position];
         }
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.refresh) {
+            Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (itemId == R.id.action_sth) {
+            Toast.makeText(this, "Doing Something", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
